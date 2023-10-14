a50076c07e06b1cd21c6c0b5f086b478d8a3491a
Roch D'Amour <roch.damour@gmail.com>
2023-10-13 23:15:59 -0400

    Base config

--- base-config.nix
+++ base-config.nix
@@ -1,8 +1,28 @@
 { lib, pkgs, ... }:
-{
+let
+  authorizedKeys = pkgs.fetchurl {
+    url = "https://github.com/notarock.keys";
+    sha256 = "sha256-csW0RDWhKWyShy3/DzmMjRZTBS84VDdxecvpSgBTo6s=";
+  };
+in {
+
   systemd.network.enable = true;
   systemd.services.sshd.wantedBy = lib.mkForce [ "multi-user.target" ];
 
-  # users.extraUsers.root.openssh.authorizedKeys.keys = [
-  # ];
+  services.openssh.enable = true;
+  users.users.root.openssh.authorizedKeys.keys =
+    pkgs.lib.splitString "\n" (builtins.readFile authorizedKeys);
+
+  virtualisation.oci-containers.containers.test.image = "library/hello-world";
+
+  users.users.notarock = {
+    isNormalUser = true;
+    home = "/home/notarock";
+    description = "Nickname for root";
+    extraGroups = [ "docker" "video" ];
+    shell = pkgs.zsh;
+    initialPassword = "Ch4ngeMoi%%%";
+    openssh.authorizedKeys.keys =
+      pkgs.lib.splitString "\n" (builtins.readFile authorizedKeys);
+  };
 }
